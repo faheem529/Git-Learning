89a6b5108889d815e7b1b7b52643a43a60f84922
Md Faheem <mdfaheem500786@gmail.com>
2025-12-07 20:47:40 +0530

    Button Added

--- GitsNew/first.js
+++ GitsNew/first.js
@@ -1,3 +1,33 @@
 const name = "Rohit";
 const age = 20;
 const balance = 1000;
+
+const account = 230;
+
+console.log("Hello");
+function updateBalance() {
+    balance += 20;
+}
+
+updateBalance();
+
+consolo.log(balance);
+
+console.log("Money honey");
+console.log("Bug final commit");
+console.log("Main hu hero tera");
+
+function showage(){
+    console.log(age);
+}
+
+showage();
+
+function showname(){
+    console.log(name);
+}
+
+showname(); 
+
+
+crossOriginIsolated.logZ(balance)
